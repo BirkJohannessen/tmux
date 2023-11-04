094c181ca1ab70efdfb4bc6d13c9c18d74a64527
Birk Johannessen <>
2023-11-04 20:57:53 +0100

    install dependency xsel

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt install xsel -y
 sudo apt install tmux -y
 git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm
 tmux source $HOME/.config/tmux/tmux.conf
