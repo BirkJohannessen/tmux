102f80a6c3bec1ea6b458d481c3fafc5a6043a23
Birk Johannessen <>
2024-09-16 08:44:43 +0200

    tmux test config

--- install.sh
+++ install.sh
@@ -4,3 +4,14 @@ sudo apt install xsel -y
 sudo apt install tmux -y
 git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm
 tmux source $HOME/.config/tmux/tmux.conf
+
+
+
+# Hack font from https://www.nerdfonts.com/font-downloads
+# copy dl link to replace below.
+# replace zip file aswell..
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip \
+&& cd ~/.local/share/fonts \
+&& unzip Hack.zip \
+&& rm Hack.zip \
+&& fc-cache -fv
